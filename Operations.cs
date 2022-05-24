368bdbf15e6f82d1da485a1e85c60a8498657261
Ambili das v s <ambili.kan@gmail.com>
2022-05-24 18:25:31 +0530

    Linked List program is created

---
+++ Operations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class Operations
+    {
+        public Node head;
+        public void Add(int Data)
+        {
+            Node newnode=new Node(Data);
+            if(this.head == null)
+            {
+                this.head = newnode;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = newnode;
+            }
+            
+        }
+        public void Display()
+        {
+            Node temp=this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("Linked List is empty!!");
+                return;
+            }
+            while(temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Program for creating Linked List");
+        }
+    }
+}
