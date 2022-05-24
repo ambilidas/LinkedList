0a8c05d1721a81d36da792e8bb7f53d3ae8ab468
Ambili das v s <ambili.kan@gmail.com>
2022-05-24 18:27:38 +0530

    Simple linked list is created

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace LinkedList
         public static void Main(string[] args)
         {
             Console.WriteLine("Program for creating Linked List");
+            Operations operationsobj = new Operations();
+            operationsobj.Add(56);
+            operationsobj.Add(30);
+            operationsobj.Add(70);
+            operationsobj.Display();
         }
     }
 }
